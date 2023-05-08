62949df54775e14ea2da2a1d1a26ffb15a4c2ad5
AJ Javadi <amirjavadi25@gmail.com>
2023-05-08 09:03:21 -0500

    updated js file

---
+++ prework-study-guide/assets/script.js
@@ -0,0 +1,37 @@
+// script js file 
+// var topics = "HTML, CSS, Git, JavaScript";
+// console.log(topics);
+
+//
+// if (0) {
+//    console.log("I'm 0!")
+// } else if (null) {
+//    console.log("I'm null!")
+// } else if (true) {
+//    console.log("I'm true!")
+// }
+// var topic = "HTML";
+//
+// if (topic === 'HTML') {
+//   console.log("Let's study HTML!");
+// } else if (topic === 'CSS') {
+//   console.log("Let's study CSS!");
+// } else if (topic === 'Git') {
+//   console.log("Let's study Git!");
+// } else if (topic === 'JavaScript') {
+//   console.log("Let's study JavaScript!");
+// } else {
+//   console.log('Please try again!');
+// }
+//
+// var shapes = ["triangle", "square", "pentagon", "circle"];
+// console.log(shapes[0]);
+//
+// for(var x = 0; x < shapes.length; x++) {
+//  console.log(shapes[x]);
+// }
+
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+for (var x = 0; x < topics.length; x++){
+	console.log(topics[x])
+};
