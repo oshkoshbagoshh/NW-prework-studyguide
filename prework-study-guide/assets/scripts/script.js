2e4d8d1d86fec52910975a15f9e18fd65d7dd00a
AJ Javadi <amirjavadi25@gmail.com>
2023-05-22 18:50:58 -0500

    fixed script.js

--- prework-study-guide/assets/scripts/script.js
+++ prework-study-guide/assets/scripts/script.js
@@ -44,23 +44,41 @@ function listTopics() {
 	}
 }
 
+// function selectTopic() {
+// 	if (topics[0] === 'HTML'){
+// 		console.log("Let's study HTML!");
+// 	} else if (topics[0] === 'CSS') {
+// 		console.log("Let's study CSS!");
+// 	} else if (topics[0] === 'Git') {
+// 		console.log("Let's study Git!");
+// 	} else if (topics[0] === 'JavaScript') {
+// 		console.log("Let's study JavaScript");
+// 	} else {
+// 		console.log('Please try again!');
+// 	}
+// }
+
 function selectTopic() {
-	if (topics[0] === 'HTML'){
+
+	if (randomTopic === 'HTML'){
 		console.log("Let's study HTML!");
-	} else if (topics[0] === 'CSS') {
-		console.log("Let's study CSS!");
-	} else if (topics[0] === 'Git') {
-		console.log("Let's study Git!");
-	} else if (topics[0] === 'JavaScript') {
-		console.log("Let's study JavaScript");
-	} else {
-		console.log('Please try again!');
+	} else if (randomTopic === 'CSS') {
+		console.log("Let's study CSS");
+	}  else if (randomTopic === 'Git') {
+			console.log("Let's study Git!"); 
+	} else if (randomTopic === 'JavaScript') {
+			console.log("Let's study JavaScript!");
+	} else  {
+		console.log("Please try again!");
 	}
+
+
 }
+
 
 console.log('Here are the topics we learned through Prework:');
 listTopics();
 console.log(listTopics());
 console.log('Which topic should we study first?');
-selectTopic('HTML');
+selectTopic();
 // selectTopic();
